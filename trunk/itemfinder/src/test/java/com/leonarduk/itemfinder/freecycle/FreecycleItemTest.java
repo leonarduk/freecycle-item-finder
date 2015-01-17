f5d1b78fdb4a2d7ac88cd5655ef894baf9c73bdd
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-01-17 23:10:25 +0000

    get full details

--- trunk/itemfinder/src/test/java/com/leonarduk/itemfinder/freecycle/FreecycleItemTest.java
+++ trunk/itemfinder/src/test/java/com/leonarduk/itemfinder/freecycle/FreecycleItemTest.java
@@ -5,9 +5,14 @@ package com.leonarduk.itemfinder.freecycle;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.adamshone.freecycle.Post;
+import com.adamshone.freecycle.PostType;
+import com.leonarduk.freecycle.FullPost;
 import com.leonarduk.itemfinder.interfaces.Item.Condition;
 
 /**
@@ -23,6 +28,7 @@ public class FreecycleItemTest {
 	private String description;
 	private Condition condition;
 	private String link;
+	private String location;
 
 	/**
 	 * @throws java.lang.Exception
@@ -36,9 +42,9 @@ public class FreecycleItemTest {
 		this.condition = Condition.REFURBISHED;
 		this.link = "https://groups.freecycle.org/group/freecycle-kingston/posts/44926473";
 
-		this.testClass = new FreecycleItem(this.name, this.quantity,
-				this.price, this.condition, this.description, this.link);
-
+		this.testClass = new FreecycleItem(new FullPost(this.location,
+				this.description, new Post(PostType.OFFER, new Date(),
+						this.name, this.link)));
 	}
 
 	/**
