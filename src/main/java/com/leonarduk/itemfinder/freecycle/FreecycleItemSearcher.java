c09d0446eea186b35e8d851201ec8416df425378
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-01-18 00:05:28 +0000

    expanded details

--- src/main/java/com/leonarduk/itemfinder/freecycle/FreecycleItemSearcher.java
+++ src/main/java/com/leonarduk/itemfinder/freecycle/FreecycleItemSearcher.java
@@ -6,18 +6,12 @@ package com.leonarduk.itemfinder.freecycle;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.derby.tools.sysinfo;
-import org.htmlparser.Node;
 import org.htmlparser.Parser;
-import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
-import org.htmlparser.util.SimpleNodeIterator;
 
 import com.adamshone.freecycle.Post;
-import com.adamshone.freecycle.PostProvider;
-import com.adamshone.freecycle.impl.FreecycleNewhamScraper;
 import com.leonarduk.freecycle.FreecycleItemScraper;
-import com.leonarduk.freecycle.FullPost;
+import com.leonarduk.freecycle.FreecycleQueryBuilder;
 import com.leonarduk.itemfinder.interfaces.Item;
 import com.leonarduk.itemfinder.interfaces.Item.Condition;
 import com.leonarduk.itemfinder.interfaces.ItemSearcher;
@@ -32,16 +26,6 @@ public class FreecycleItemSearcher implements ItemSearcher {
 
 	public FreecycleItemSearcher(String town) {
 		this.town = town;
-	}
-
-	public FreecycleItemScraper getFreecycleParser() throws ParserException {
-
-		String freecycleUrl = "http://groups.freecycle.org/freecycle-"
-				+ town
-				+ "/posts/all?page=1&amp;resultsperpage=50&amp;showall=off&amp;include_offers=off&amp;include_wanteds=off&amp;include_receiveds=off&amp;include_takens=on";
-		Parser parser = new Parser(freecycleUrl);
-		FreecycleItemScraper postProvider = new FreecycleItemScraper(parser);
-		return postProvider;
 	}
 
 	/*
@@ -107,20 +91,26 @@ public class FreecycleItemSearcher implements ItemSearcher {
 	 * String)
 	 */
 	@Override
-	public List<Item> findItems(String searchTerm) {
-		FreecycleItemScraper parser;
+	public List<Item> findItems(String searchTerm) throws ItemFinderException {
+
 		List<Item> items = new ArrayList<Item>();
 		try {
-			parser = getFreecycleParser();
-			List<Post> posts = parser.getPosts();
-			for (Post post : posts) {
-				items.add(new FreecycleItem(parser.getFullPost(post)));
-			}
+			String url = new FreecycleQueryBuilder(town).setSearchWords(
+					searchTerm).build();
+			getPosts(items, url);
 		} catch (ParserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new ItemFinderException(e.getMessage());
 		}
 		return items;
+	}
+
+	public void getPosts(List<Item> items, String url) throws ParserException {
+		Parser parser = new Parser(url);
+		FreecycleItemScraper postProvider = new FreecycleItemScraper(parser);
+		List<Post> posts = postProvider.getPosts();
+		for (Post post : posts) {
+			items.add(new FreecycleItem(postProvider.getFullPost(post)));
+		}
 	}
 
 }
