dfd2bf71f0a42c7a84a5fb1f7d070bef42dd8f89
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-01-28 23:43:06 +0000

    code style fixes

--- src/main/java/com/leonarduk/itemfinder/ItemFinderMain.java
+++ src/main/java/com/leonarduk/itemfinder/ItemFinderMain.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2002-2013 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.leonarduk.itemfinder;
 
@@ -36,65 +34,75 @@ import com.leonarduk.itemfinder.query.QueryReporter;
 /**
  * Starts the Spring Context and will initialize the Spring Integration routes.
  *
+ *
  * @author Stephen Leonard
- * @since 1.0
+ * @since 28 Jan 2015
+ *
+ * @version $Author:: $: Author of last commit
+ * @version $Rev:: $: Revision of last commit
+ * @version $Date:: $: Date of last commit
  *
  */
 public final class ItemFinderMain {
 
-	private static final Logger LOGGER = Logger.getLogger(ItemFinderMain.class);
+	/** The Constant LOGGER. */
+	private static final Logger	LOGGER	= Logger.getLogger(ItemFinderMain.class);
 
-	public static void main(final String[] args) throws ItemFinderException,
-			Exception {
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 * @throws ItemFinderException
+	 *             the item finder exception
+	 * @throws Exception
+	 *             the exception
+	 */
+	public static void main(final String[] args) throws ItemFinderException, Exception {
 		// JBidWatch.main(new String[]{});
 
-		Config config = new Config("itemfinder.properties");
-		String[] searches = config.getArrayProperty("freecycle.search.terms");
+		final Config config = new Config("itemfinder.properties");
+		final String[] searches = config.getArrayProperty("freecycle.search.terms");
 
-		String[] groupNames = config
-				.getArrayProperty("freecycle.search.groups");
-		FreecycleGroups[] groups = new FreecycleGroups[groupNames.length];
+		final String[] groupNames = config.getArrayProperty("freecycle.search.groups");
+		final FreecycleGroups[] groups = new FreecycleGroups[groupNames.length];
 		for (int i = 0; i < groups.length; i++) {
 			groups[i] = FreecycleGroups.valueOf(groupNames[i]);
 		}
-		Formatter formatter = new HtmlFormatter();
-		StringBuilder emailBody = new StringBuilder();
+		final Formatter formatter = new HtmlFormatter();
+		final StringBuilder emailBody = new StringBuilder();
 
-		EntityManagerFactory emf = Persistence
-				.createEntityManagerFactory("ReportableItem");
-		EntityManager em = emf.createEntityManager();
+		final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ReportableItem");
+		final EntityManager em = emf.createEntityManager();
 		emailBody.append(QueryReporter.runReport(searches, groups,
-				config.getIntegerProperty("freecycle.search.period"),
-				formatter, em));
-		emailBody.append(QueryReporter.runReport(new String[] { "" }, groups,
-				1, formatter, em));
+				config.getIntegerProperty("freecycle.search.period"), formatter, em));
+		emailBody.append(QueryReporter.runReport(new String[] { "" }, groups, 1, formatter, em));
 
-		String[] toEmail = config.getArrayProperty("freecycle.email.to");
+		final String[] toEmail = config.getArrayProperty("freecycle.email.to");
 		final String user = config.getProperty("freecycle.email.user");
-		String server = config.getProperty("freecycle.email.server");
+		final String server = config.getProperty("freecycle.email.server");
 		final String password = config.getProperty("freecycle.email.password");
-		String port = config.getProperty("freecycle.email.port");
+		final String port = config.getProperty("freecycle.email.port");
 
-		EmailSender emailSender = new EmailSender();
+		final EmailSender emailSender = new EmailSender();
 
-		EmailSession session = new EmailSession(user, password, server, port);
-		emailSender.sendMessage(
-				config.getProperty("freecycle.email.from.email"),
-				config.getProperty("freecycle.email.from.name"),
-				"Matching Freecycle items found", emailBody.toString(), true,
-				session, toEmail);
+		final EmailSession session = new EmailSession(user, password, server, port);
+		emailSender.sendMessage(config.getProperty("freecycle.email.from.email"),
+				config.getProperty("freecycle.email.from.name"), "Matching Freecycle items found",
+				emailBody.toString(), true, session, toEmail);
 	}
 
 	/**
-	 * Load the Spring Integration Application Context
+	 * Load the Spring Integration Application Context.
 	 *
 	 * @param args
 	 *            - command line arguments
 	 */
 	public static void main2(final String... args) {
 
-		if (LOGGER.isInfoEnabled()) {
-			LOGGER.info("\n========================================================="
+		if (ItemFinderMain.LOGGER.isInfoEnabled()) {
+			ItemFinderMain.LOGGER
+			.info("\n========================================================="
 					+ "\n                                                         "
 					+ "\n          Welcome to Spring Integration!                 "
 					+ "\n                                                         "
@@ -113,8 +121,9 @@ public final class ItemFinderMain {
 
 		final Scanner scanner = new Scanner(System.in);
 
-		if (LOGGER.isInfoEnabled()) {
-			LOGGER.info("\n========================================================="
+		if (ItemFinderMain.LOGGER.isInfoEnabled()) {
+			ItemFinderMain.LOGGER
+			.info("\n========================================================="
 					+ "\n                                                         "
 					+ "\n    Please press 'q + Enter' to quit the application.    "
 					+ "\n                                                         "
@@ -125,14 +134,17 @@ public final class ItemFinderMain {
 			// Do nothing unless user presses 'q' to quit.
 		}
 
-		if (LOGGER.isInfoEnabled()) {
-			LOGGER.info("Exiting application...bye.");
+		if (ItemFinderMain.LOGGER.isInfoEnabled()) {
+			ItemFinderMain.LOGGER.info("Exiting application...bye.");
 		}
 
 		System.exit(0);
 
 	}
 
+	/**
+	 * Instantiates a new item finder main.
+	 */
 	private ItemFinderMain() {
 
 	}
