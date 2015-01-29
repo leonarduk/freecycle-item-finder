1cfbb6caced0d395dae455d606c9b222723f4608
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-01-29 23:00:06 +0000

    delete

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-java -jar target/itemfinder-1.0.0.BUILD-SNAPSHOT-jar-with-dependencies.jar 
+DIR=/home/stephen/workspace/luk/trunk/itemfinder/target/
+
+java -jar $DIR/itemfinder-1.0.0.BUILD-SNAPSHOT-jar-with-dependencies.jar 
