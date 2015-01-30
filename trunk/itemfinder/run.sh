b6aabab266c441a2ac18f01533cdb68b301ab7df
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-01-30 00:43:49 +0000

    rerun

--- trunk/itemfinder/run.sh
+++ trunk/itemfinder/run.sh
@@ -1,3 +1,5 @@
+kill `ps -ef  | grep itemfinder | grep -v grep | awk -e ' { print $2 } '`
+
 cd /home/stephen/workspace/luk/trunk/itemfinder
 java -jar target/itemfinder-1.1.0-jar-with-dependencies.jar
 
