446a5c49f27a87e1e5ab62c6932d2e977c327c48
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-01-31 23:09:57 +0000

    update version

--- restart.sh
+++ restart.sh
@@ -8,5 +8,5 @@ else
 echo "Not Running - will start"
 fi
 cd /home/stephen/workspace/luk/trunk/itemfinder
-nohup java -jar target/itemfinder-1.1.0-jar-with-dependencies.jar & >/dev/null
+nohup java -jar target/itemfinder-1.1.1-jar-with-dependencies.jar & >/dev/null
 
