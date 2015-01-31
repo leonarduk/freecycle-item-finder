c60ff5e7970fb732ef6950e544280ae731e164e6
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-01-31 23:04:50 +0000

    new script

--- trunk/itemfinder/kill.sh
+++ trunk/itemfinder/kill.sh
@@ -2,7 +2,7 @@
 PID=`ps -ef  | grep itemfinder | grep -v grep | awk  ' { print $2 } '`
 
 if [[ $PID != '' ]]; then
-echo "Already running - kill it and restart"
+echo "Already running - kill it"
 kill $PID
 else
 echo "Not Running"
