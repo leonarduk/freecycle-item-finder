350d14e5de8353251197f70325c60073512cf294
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-02-25 22:11:22 +0000

    try for jenkins

--- trunk/itemfinder/restart.sh
+++ trunk/itemfinder/restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-PID=`ps -ef  | grep itemfinder | grep -v grep | awk  ' { print $2 } '`
+JAR=itemfinder-1.1.2-jar-with-dependencies.jar
+PID=`ps -ef  | grep $JAR | grep -v grep | awk  ' { print $2 } '`
 
 if [[ $PID != '' ]]; then
 echo "Already running - kill it and restart"
@@ -8,4 +9,4 @@ else
 echo "Not Running - will start"
 fi
 cd /home/stephen/workspace/luk/trunk/itemfinder
-java -jar target/itemfinder-1.1.2-jar-with-dependencies.jar 
+java -jar target/$JAR
