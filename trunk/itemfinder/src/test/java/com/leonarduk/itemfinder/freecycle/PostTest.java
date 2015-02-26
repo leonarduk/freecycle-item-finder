fcdb116d8628680630b820f16c77ec549518b00c
leonarduk <leonarduk@ae296b99-704d-4723-b7e8-219d2c3e5e5c>
2015-02-26 22:10:21 +0000

    working again

---
+++ trunk/itemfinder/src/test/java/com/leonarduk/itemfinder/freecycle/PostTest.java
@@ -0,0 +1,132 @@
+/**
+ * All rights reserved. @Leonard UK Ltd.
+ */
+package com.leonarduk.itemfinder.freecycle;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The Class PostTest.
+ *
+ * @author stephen
+ * @version $Author: $: Author of last commit
+ * @version $Rev: $: Revision of last commit
+ * @version $Date: $: Date of last commit
+ * @since 26 Feb 2015
+ */
+public class PostTest {
+
+    private PostType postTypeValue;
+    private Date postDateValue;
+    private String titleValue;
+    private String linkValue;
+    private FreecycleGroup group;
+    private Post post;
+    private int postId;
+
+    /**
+     * Sets the up.
+     *
+     * @throws Exception
+     *             the exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        this.postDateValue = new Date();
+        this.postTypeValue = PostType.OFFER;
+        this.titleValue = "Kids Snow Boots";
+        this.postId = 45736770;
+        this.linkValue =
+                "https://groups.freecycle.org/group/freecycle-kingston/posts/"
+                        + this.postId + "/Kids%20Snow%20Boots";
+        this.group = FreecycleGroup.kingston;
+        this.post =
+                new Post(this.postTypeValue, this.postDateValue,
+                        this.titleValue, this.linkValue, this.group);
+    }
+
+    /**
+     * Tear down.
+     *
+     * @throws Exception
+     *             the exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test get date.
+     */
+    @Test
+    public final void testGetDate() {
+        Assert.assertEquals(this.postDateValue, this.post.getDate());
+    }
+
+    /**
+     * Test get freecycle group.
+     */
+    @Test
+    public final void testGetFreecycleGroup() {
+        Assert.assertEquals(this.group, this.post.getFreecycleGroup());
+    }
+
+    /**
+     * Test get link.
+     */
+    @Test
+    public final void testGetLink() {
+        Assert.assertEquals(this.linkValue, this.post.getLink());
+    }
+
+    /**
+     * Test get post id.
+     */
+    @Test
+    public final void testGetPostId() {
+        Assert.assertEquals(this.postId, this.post.getPostId());
+    }
+
+    /**
+     * Test get post type.
+     */
+    @Test
+    public final void testGetPostType() {
+        Assert.assertEquals(this.postTypeValue, this.post.getPostType());
+    }
+
+    /**
+     * Test get text.
+     */
+    @Test
+    public final void testGetText() {
+        Assert.assertEquals(this.titleValue, this.post.getText());
+    }
+
+    /**
+     * Test to post.
+     */
+    @Test
+    public final void testToPost() {
+        final String expected =
+                "OFFER: Kids Snow Boots (https://groups.freecycle.org/group/freecycle-kingston/posts/45736770/Kids%20Snow%20Boots)";
+        Assert.assertEquals(expected, this.post.toPost());
+    }
+
+    /**
+     * Test to string.
+     */
+    @Test
+    public final void testToString() {
+        final String expected =
+                this.postDateValue
+                        + ": OFFER: Kids Snow Boots (https://groups.freecycle.org/group/freecycle-kingston/posts/45736770/Kids%20Snow%20Boots)";
+        Assert.assertEquals(expected, this.post.toString());
+    }
+
+}
--- trunk/itemfinder/src/test/java/com/leonarduk/itemfinder/query/SearchReporterTest.java
+++ trunk/itemfinder/src/test/java/com/leonarduk/itemfinder/query/SearchReporterTest.java
@@ -3,10 +3,21 @@
  */
 package com.leonarduk.itemfinder.query;
 
+import java.util.concurrent.ExecutionException;
+
+import javax.persistence.EntityManager;
+
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.leonarduk.itemfinder.ItemFinderException;
+import com.leonarduk.itemfinder.MockObjectGenerator;
+import com.leonarduk.itemfinder.format.Formatter;
+import com.leonarduk.itemfinder.format.HtmlFormatter;
+import com.leonarduk.itemfinder.freecycle.FreecycleConfig;
+import com.leonarduk.itemfinder.freecycle.FreecycleGroup;
 
 /**
  * The Class SearchReporterTest.
@@ -19,48 +30,86 @@ import org.junit.Test;
  */
 public class SearchReporterTest {
 
-	/** The Constant LOGGER. */
-	static final Logger LOGGER = Logger.getLogger(SearchReporterTest.class);
+    /** The Constant LOGGER. */
+    static final Logger LOGGER = Logger.getLogger(SearchReporterTest.class);
+    private FreecycleConfig config;
+    private String[] searches;
+    private FreecycleGroup[] groups;
+    private Formatter formatter;
+    private EntityManager em;
+    private QueryReporter reporter;
+    private boolean failIfEmpty;
 
-	/**
-	 * Sets the up.
-	 *
-	 * @throws Exception
-	 *             the exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
+    /**
+     * Sets the up.
+     *
+     * @throws Exception
+     *             the exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        this.config = MockObjectGenerator.createFreecycleConfig();
+        this.searches = new String[] {
+                "bed", "desk"
+        };
+        this.groups = new FreecycleGroup[] {
+                FreecycleGroup.kingston
+        };
+        this.reporter = new QueryReporter();
+        this.failIfEmpty = false;
+        this.formatter = new HtmlFormatter();
+        this.em = MockObjectGenerator.getMockEntityManager();
 
-	/**
-	 * Tear down.
-	 *
-	 * @throws Exception
-	 *             the exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
+    }
 
-	/**
-	 * Test generate report.
-	 */
-	@Test
-	public final void testGenerateReport() {
-	}
+    /**
+     * Tear down.
+     *
+     * @throws Exception
+     *             the exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	/**
-	 * Test generate search.
-	 */
-	@Test
-	public final void testGenerateSearch() {
-	}
+    /**
+     * Test generate report.
+     *
+     * @throws ItemFinderException
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public final void testGenerateReport()
+            throws InterruptedException,
+            ExecutionException,
+            ItemFinderException {
+        SearchReporter.generateReport(this.config, this.searches, this.groups,
+                this.formatter, this.em, this.reporter, this.failIfEmpty);
 
-	/**
-	 * Test send report.
-	 */
-	@Test
-	public final void testSendReport() {
-	}
+    }
+
+    /**
+     * Test generate search.
+     *
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    // @Test
+    // public final void testGenerateSearch()
+    // throws InterruptedException,
+    // ExecutionException {
+    // SearchReporter.generateSearch(this.config, this.searches, this.groups,
+    // this.formatter, this.em, this.reporter, this.failIfEmpty);
+    // }
+
+    /**
+     * Test send report.
+     */
+    @Test
+    public final void testSendReport() {
+        final String emailBody = "test email text";
+        SearchReporter.sendReport(this.config, emailBody);
+    }
 
 }
