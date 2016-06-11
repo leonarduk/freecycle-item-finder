4023730da84a44f16ea4bcc0e128128307dac49f
Stephen <github@leonarduk.com>
2016-06-11 22:01:01 +0100

    #18 clean out logo from emails

--- run.bat
+++ run.bat
@@ -1,11 +1,15 @@
-java -jar target/freecycle-item-finder-1.0.1-SNAPSHOT-jar-with-dependencies.jar ^
--Dfreecycle.email.user=bookkeeper@leonarduk.com ^
+set JAVA_TOOL_OPTIONS=-Dfreecycle.email.user=bookkeeper@leonarduk.com ^
 -Dfreecycle.email.server=oakland.acenet.us ^
 -Dfreecycle.email.server.type=imap ^
--Dfreecycle.email.password=c00kTHEbooks ^
+-Dfreecycle.email.password=c00kTHEb00ks ^
 -Dfreecycle.email.port=465 ^
 -Dfreecycle.email.from.email=FreecycleItemFinder@leonarduk.com ^
 -Dfreecycle.email.from.name=FreecycleItemFinder ^
--Dfreecycle.email.to=steveleonard11@gmail.com,lucyleonard@hotmail.com
+-Dfreecycle.email.to=batch@leonarduk.com,lucyleonard@hotmail.com ^
+-Dfreecycle.search.interval=5 ^
+-Dfreecycle.search.groups=kingston,elmbridge,richmond,wandsworth,merton,hammersmith,ealing,lambeth,epsom ^
+-Dfreecycle.search.period=7 ^
+-Dfreecycle.search.limit=10 ^
+-Dfreecycle.search.terms=bugaboo,Playhouse,Roof rack,Food processor,Flatscreen TV,LCD TV,tiffany,fish tank, aquarium,juicer,london stock,yellow bricks,fire wood,kindling,logs,Boys clothes,toddler clothes,Childs bookcase,childrens bookcase,Toddler trike,childs trike,dresser,coatstand,coat stand,water butt,single bed,mirror,baby walker
 
-
+java -jar target/freecycle-item-finder-1.0.1-jar-with-dependencies.jar 
